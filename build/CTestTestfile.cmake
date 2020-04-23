2010b8031cc679a5e60b7e4ad44793508040f446
Francesco Alongi <alongi.francesco96@gmail.com>
2020-04-23 17:13:37 +0200

    Initializing repository

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/francesco/first_rob_proj/src
+# Build directory: /home/francesco/first_rob_proj/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("project1")
---
+++ build/project1/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/francesco/first_rob_proj/src/project1
+# Build directory: /home/francesco/first_rob_proj/build/project1
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
